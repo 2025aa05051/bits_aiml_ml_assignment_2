15b982a81c50dd4d56b4f48f48c57eebe6640d43
2025aa05051 <2025aa05051@wilp.bits-pilani.ac.in>
2026-01-12 14:54:47 +0530

    updates models and Metrices

--- model/random_forest.py
+++ model/random_forest.py
@@ -2,4 +2,10 @@ from sklearn.ensemble import RandomForestClassifier
 from datapreperation.config import RANDOM_STATE
 
 def build_model():
-    return RandomForestClassifier(n_estimators=300, random_state=RANDOM_STATE)
+     return RandomForestClassifier(
+        n_estimators=250,       # ↓ from 300
+        max_depth=12,           # limits tree size
+        min_samples_leaf=2,     # reduces overfitting + size
+        random_state=RANDOM_STATE,
+        n_jobs=-1
+    )
